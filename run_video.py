02345109a0c498caf82213842665cdb3019633e4
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-08-08 10:12:15 +0900

    refactor

--- run_video.py
+++ run_video.py
@@ -1,4 +1,5 @@
 import argparse
+from importlib.resources import path
 import logging
 import time
 import csv
@@ -21,8 +22,8 @@ fps_time = 0
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='bigwind.mov')
-    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
-    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--resolution', type=str, default='464x400', help='network input resolution. default=464x400')
+    parser.add_argument('--model', type=str, default='mobilenet_v2_large', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
     parser.add_argument('--show-process', type=bool, default=False,
                         help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
@@ -31,17 +32,19 @@ if __name__ == '__main__':
     # logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resolution)
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+
     cap = cv2.VideoCapture(args.video)
-    cap.set(cv2.CAP_PROP_FPS, int(30))
+    # cap.set(cv2.CAP_PROP_FPS, int(30))
 
     if cap.isOpened() is False:
         print("Error opening video stream or file")
     video_landmarks = []
+    fps_list = []
     while cap.isOpened():
         ret_val, image = cap.read()
         if not ret_val:
-            continue
-        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size = 4.0)
+            break
+        humans = e.inference(image, resize_to_default = True, upsample_size = 4.0)
         if len(humans) > 0:
             bodyParts = humans[0].body_parts
             frame_landmarks = []
@@ -60,7 +63,15 @@ if __name__ == '__main__':
                 csvwriter.writerows(video_landmarks)
 
             # print(video_landmarks)
-        cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        t2 = time.time()
+        fps = int(1 / (t2 - fps_time))
+        # print(t2-fps)
+        fps_list.append(fps)
+
+        if len(fps_list) > 20:
+            fps_mean = int(sum(fps_list)/len(fps_list))
+
+            cv2.putText(image, "FPS: " f"{fps_mean}", (10, 60),  cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 2)
         cv2.resize(image, (1920, 1080))
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
@@ -69,7 +80,7 @@ if __name__ == '__main__':
     cv2.destroyAllWindows()
 # logger.debug('finished+')
 # For single process
-highProcess = psutil.pids()[-1]
-p = psutil.Process(highProcess)
-print(p.name)
-p.terminate()
+# highProcess = psutil.pids()[-1]
+# p = psutil.Process(highProcess)
+# print(p.name)
+# p.terminate()
--- run_webcam.py
+++ run_webcam.py
@@ -12,17 +12,10 @@ import multiprocessing
 
 from numba import cuda
 tf.keras.backend.clear_session()
+import json
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
-
-logger = logging.getLogger('TfPoseEstimator-WebCam')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
 
 def str2bool(v):
@@ -38,49 +31,93 @@ def main():
                         help='if provided, resize heatmaps before they are post-processed. default=1.0')
 
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--show-process', type=bool, default=False,
-                        help='for debug purpose, if enabled, speed for inference is dropped.')
     
     parser.add_argument('--tensorrt', type=str, default="False",
                         help='for tensorrt process.')
+    parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
     args = parser.parse_args()
 
-    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resize)
     if w > 0 and h > 0:
         e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h), trt_bool=str2bool(args.tensorrt))
     else:
         e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368), trt_bool=str2bool(args.tensorrt))
-    logger.debug('cam read+')
-
     fps_time = 0
 
     cam = cv2.VideoCapture(args.camera)
     ret_val, image = cam.read()
-    # logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
-
+    counter = 1
     while True:
         ret_val, image = cam.read()
 
-        logger.debug('image process+')
         humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
 
-        # Print keypoints
-        # keypoints = str(str(str(humans[0]).split('BodyPart:')[1:]).split('-')).split(' score=')
-        # print(keypoints)
-        # keypts_array = np.array(keypoints_list)
-        # keypts_array = keypts_array*(image.shape[1],image.shape[0])
-        # keypts_array = keypts_array.astype(int)
+        frame_landmarks = {"person_a":{}, "person_b" : {}, "person_c" : {}, "person_d" : {}}
+        if len(humans) == 0:
+            continue
 
-        # logger.debug('postprocess+')
-        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        elif len(humans) == 1:
+            first_person = humans[0].body_parts
+            
+            frame = {}
+            frame_list = []
+            for _, v_a in first_person.items():
+                body_index_a = v_a.part_idx
+                x_a, y_a = v_a.x, v_a.y
+                frame[body_index_a] = [round(x_a, 3), round(y_a, 3)]
+                frame_list.append(frame)
+                frame_landmarks["person_a"] = frame_list
+            # with open("frame_landmarks.json", "w") as f:
+            #     json.dump(frame_landmarks, f)
+            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        elif len(humans) == 2:
+            first_person = humans[0].body_parts
+            second_person = humans[1].body_parts
+            # third_person = humans[2].body_parts
+            # fourth_person = humans[3].body_parts
 
-        cv2.resize(image, (1080, 720), cv2.INTER_LINEAR)
-        # logger.debug('show+')
+            for _, v_a in first_person.items():
+                body_index_a = v_a.part_idx
+                x_a, y_a = v_a.x, v_a.y
+                frame[body_index_a] = [round(x_a, 3), round(y_a, 3)]
+                frame_list.append(frame)
+                frame_landmarks["person_a"] = frame_list
+            # with open("frame_landmarks.json", "w") as f:
+            #     json.dump(frame_landmarks, f)
+                
+            for _, v_b in second_person.items():
+                body_index_b = v_b.part_idx
+                x_b, y_b = v_b.x, v_b.y
+                frame[body_index_b] = [round(x_b, 3), round(y_b, 3)]
+                frame_list.append(frame)
+                frame_landmarks["person_b"] = frame_list
+            # with open("frame_landmarks.json", "w") as f:
+            #     json.dump(frame_landmarks, f)
+
+            # for _, v_c in third_person.items():
+            #     body_index_c = v_c.part_idx
+            #     x_c, y_c = v_c.x, v_c.y
+
+            # for _, v_d in fourth_person.items():q
+            #     body_index_d = v_d.part_idx
+            #     x_d, y_d = v_d.x, v_d.y 
+            
+            # frame_landmarks["person_c"][body_index_c] = [round(x_c, 3), round(y_c, 3)]
+            # frame_landmarks["person_d"][body_index_d] = [round(x_d, 3), round(y_d, 3)]
+            # with open("frame_landmarks.json", "w") as f:
+            #     json.dump(frame_landmarks, f)
+            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        else:
+            image = np.zeros(image.shape)
+            image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+        cv2.resize(image, (1080, 720), cv2.INTER_CUBIC)
+        counter += 1
         cv2.putText(image,
                     "FPS: %f" % (1.0 / (time.time() - fps_time)),
                     (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     (0, 255, 0), 2)
+        cv2.putText(image, f"Persons detected = {len(humans)}", (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1, (127, 255, 255), 2)
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
         if cv2.waitKey(1) & 0xFF == ord("q"):
